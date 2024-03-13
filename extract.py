54b7dbd7aee55ea1af431bb5887fa0cba48c1b86
sunnyineverywhere <sunnyinsummer@ewhain.net>
2024-03-14 01:17:04 +0900

    chore: 프로젝트 세팅

---
+++ extract.py
@@ -0,0 +1,5 @@
+"""
+https://ddragon.leagueoflegends.com/cdn/12.1.1/data/ko_KR/champion/Aatrox.json
+
+각 버전별 패치노트를 크롤링하는 코드
+"""
---
+++ init.py
@@ -0,0 +1,5 @@
+"""
+https://ddragon.leagueoflegends.com/cdn/12.1.1/data/ko_KR/champion/Aatrox.json
+
+12.1 버전을 챔피언 초기값으로 데이터를 패치하는 코드
+"""
