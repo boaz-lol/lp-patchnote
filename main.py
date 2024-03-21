c4daae95931cfe2f9a47657e28668302b9b66eb3
sunnyineverywhere <sunnyinsummer@ewhain.net>
2024-03-21 17:03:52 +0900

    feat(main.py): 리그오브레전드 패치노트 beautifulsoup 크롤링

--- main.py
+++ main.py
@@ -1,3 +1,44 @@
 """
 정기적으로 ETL을 수행하는 패치노트
 """
+
+import requests
+from bs4 import BeautifulSoup, NavigableString, Tag
+
+url = 'https://www.leagueoflegends.com/ko-kr/news/game-updates/patch-14-4-notes/#patch-champions'
+
+response = requests.get(url)
+
+if response.status_code == 200:
+    html = response.text
+    soup = BeautifulSoup(html, 'html.parser')
+
+    container = soup.select_one('#patch-notes-container')
+    
+    champions = []
+    isStarted = False
+    for c in container:
+        if c.name == 'header':
+            if c.select_one('h2').text == '챔피언': isStarted = True
+            else: isStarted = False
+
+        if isStarted == True and isinstance(c, Tag):
+            champions.append(c)
+
+    for c in champions:
+        name  = ""
+        updated_list = []
+        updated_detail = []
+        if c.select_one('h3'):
+            name = c.select_one('h3').find('a').text
+            
+            updated_list.append(c.select('h4').text)
+            updated_detail.append(c.select('ul'))
+            
+            for title, detail in updated_list, updated_detail:
+                print(title)
+                print(detail)
+            
+        
+else: 
+    print(response.status_code)
